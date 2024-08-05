c0045ae0ab35e5c1796a1632ff6eb5b1e2151980
sdlab_ta <sdlab2024@icloud.com>
2024-08-05 14:42:44 +0900

    フィールドにDateTime型の”追加日時”追加 追加が古い順、新しい順に並び替え機能追加

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,13 @@ void main() {
 class Friend {
   String name;
   String grade;
-  String meetPlace; // 今回変更：知り合った場所を表すプロパティを追加
-  String instagramId; // 今回変更：インスタIDを表すプロパティを追加
-  String club; // 今回変更：部活を表すプロパティを追加
-  String hobby; // 今回変更：趣味を表すプロパティを追加
+  String meetPlace;
+  String instagramId;
+  String club;
+  String hobby;
+  DateTime addedDate; // 追加：追加日を表すプロパティを追加
 
-  Friend(this.name, this.grade, this.meetPlace, this.instagramId, this.club, this.hobby); // 今回変更：コンストラクタを更新
+  Friend(this.name, this.grade, this.meetPlace, this.instagramId, this.club, this.hobby, this.addedDate); // 追加：コンストラクタを更新
 }
 
 class FriendListApp extends StatelessWidget {
@@ -38,6 +39,7 @@ class FriendListPage extends StatefulWidget {
 
 class _FriendListPageState extends State<FriendListPage> {
   List<Friend> friends = [];
+  bool _isSortedByNewest = true; // 追加：新しい順か古い順かを判定するフラグ
 
   @override
   void initState() {
@@ -50,11 +52,11 @@ class _FriendListPageState extends State<FriendListPage> {
       context: context,
       builder: (BuildContext context) {
         String name = '';
-        String grade = ''; // 今回変更：学年用の変数
-        String meetPlace = ''; // 今回変更：知り合った場所用の変数
-        String instagramId = ''; // 今回変更：インスタID用の変数
-        String club = ''; // 今回変更：部活用の変数
-        String hobby = ''; // 今回変更：趣味用の変数
+        String grade = '';
+        String meetPlace = '';
+        String instagramId = '';
+        String club = '';
+        String hobby = '';
 
         return AlertDialog(
           title: Text('友達を追加'),
@@ -62,18 +64,18 @@ class _FriendListPageState extends State<FriendListPage> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               _buildTextField('名前', (value) => name = value),
-              _buildTextField('学年', (value) => grade = value), // 今回変更：学年入力フィールド
-              _buildTextField('知り合った場所', (value) => meetPlace = value), // 今回変更：知り合った場所入力フィールド
-              _buildTextField('インスタID', (value) => instagramId = value), // 今回変更：インスタID入力フィールド
-              _buildTextField('部活', (value) => club = value), // 今回変更：部活入力フィールド
-              _buildTextField('趣味', (value) => hobby = value), // 今回変更：趣味入力フィールド
+              _buildTextField('学年', (value) => grade = value),
+              _buildTextField('知り合った場所', (value) => meetPlace = value),
+              _buildTextField('インスタID', (value) => instagramId = value),
+              _buildTextField('部活', (value) => club = value),
+              _buildTextField('趣味', (value) => hobby = value),
             ],
           ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
                 setState(() {
-                  friends.add(Friend(name, grade, meetPlace, instagramId, club, hobby)); // 今回変更：Friendオブジェクト作成時に新しい情報を使用
+                  friends.add(Friend(name, grade, meetPlace, instagramId, club, hobby, DateTime.now())); // 追加：追加日を設定
                   _saveFriends(); //友達を追加した後に保存
                 });
                 Navigator.of(context).pop();
@@ -97,11 +99,11 @@ class _FriendListPageState extends State<FriendListPage> {
       context: context,
       builder: (BuildContext context) {
         String name = friends[index].name;
-        String grade = friends[index].grade; // 今回変更：学年の変数
-        String meetPlace = friends[index].meetPlace; // 今回変更：知り合った場所の変数
-        String instagramId = friends[index].instagramId; // 今回変更：インスタIDの変数
-        String club = friends[index].club; // 今回変更：部活の変数
-        String hobby = friends[index].hobby; // 今回変更：趣味の変数
+        String grade = friends[index].grade;
+        String meetPlace = friends[index].meetPlace;
+        String instagramId = friends[index].instagramId;
+        String club = friends[index].club;
+        String hobby = friends[index].hobby;
 
         return AlertDialog(
           title: Text('友達を編集'),
@@ -109,18 +111,18 @@ class _FriendListPageState extends State<FriendListPage> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               _buildTextField('名前', (value) => name = value, initialValue: name),
-              _buildTextField('学年', (value) => grade = value, initialValue: grade), // 今回変更：学年入力フィールド
-              _buildTextField('知り合った場所', (value) => meetPlace = value, initialValue: meetPlace), // 今回変更：知り合った場所入力フィールド
-              _buildTextField('インスタID', (value) => instagramId = value, initialValue: instagramId), // 今回変更：インスタID入力フィールド
-              _buildTextField('部活', (value) => club = value, initialValue: club), // 今回変更：部活入力フィールド
-              _buildTextField('趣味', (value) => hobby = value, initialValue: hobby), // 今回変更：趣味入力フィールド
+              _buildTextField('学年', (value) => grade = value, initialValue: grade),
+              _buildTextField('知り合った場所', (value) => meetPlace = value, initialValue: meetPlace),
+              _buildTextField('インスタID', (value) => instagramId = value, initialValue: instagramId),
+              _buildTextField('部活', (value) => club = value, initialValue: club),
+              _buildTextField('趣味', (value) => hobby = value, initialValue: hobby),
             ],
           ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
                 setState(() {
-                  friends[index] = Friend(name, grade, meetPlace, instagramId, club, hobby); // 今回変更：Friendオブジェクトの更新
+                  friends[index] = Friend(name, grade, meetPlace, instagramId, club, hobby, friends[index].addedDate); // 追加：追加日は変更しない
                   _saveFriends(); //友達を編集した後に保存
                 });
                 Navigator.of(context).pop();
@@ -162,11 +164,12 @@ class _FriendListPageState extends State<FriendListPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> friendList = friends.map((friend) => json.encode({
       'name': friend.name,
-      'grade': friend.grade, // 今回変更：学年の保存
-      'meetPlace': friend.meetPlace, // 今回変更：知り合った場所の保存
-      'instagramId': friend.instagramId, // 今回変更：インスタIDの保存
-      'club': friend.club, // 今回変更：部活の保存
-      'hobby': friend.hobby, // 今回変更：趣味の保存
+      'grade': friend.grade,
+      'meetPlace': friend.meetPlace,
+      'instagramId': friend.instagramId,
+      'club': friend.club,
+      'hobby': friend.hobby,
+      'addedDate': friend.addedDate.toIso8601String(), // 追加：追加日を保存
     })).toList();
     await prefs.setStringList('friendList', friendList);
   }
@@ -181,15 +184,24 @@ class _FriendListPageState extends State<FriendListPage> {
           Map<String, dynamic> friendData = json.decode(item);
           return Friend(
             friendData['name'],
-            friendData['grade'], // 今回変更：学年の読み込み
-            friendData['meetPlace'], // 今回変更：知り合った場所の読み込み
-            friendData['instagramId'], // 今回変更：インスタIDの読み込み
-            friendData['club'], // 今回変更：部活の読み込み
-            friendData['hobby'], // 今回変更：趣味の読み込み
+            friendData['grade'],
+            friendData['meetPlace'],
+            friendData['instagramId'],
+            friendData['club'],
+            friendData['hobby'],
+            DateTime.parse(friendData['addedDate']), // 追加：追加日を読み込み
           );
         }).toList();
+        _sortFriends(); // 追加：読み込み後に並び替え
       });
     }
+  }
+
+  // 追加：友達リストを新しい順か古い順に並び替えるメソッド
+  void _sortFriends() {
+    friends.sort((a, b) => _isSortedByNewest
+        ? b.addedDate.compareTo(a.addedDate)
+        : a.addedDate.compareTo(b.addedDate));
   }
 
   @override
@@ -198,6 +210,17 @@ class _FriendListPageState extends State<FriendListPage> {
       appBar: AppBar(
         title: Text('友達リスト'),
         backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(_isSortedByNewest ? Icons.arrow_downward : Icons.arrow_upward), // 追加：並び替えアイコン
+            onPressed: () {
+              setState(() {
+                _isSortedByNewest = !_isSortedByNewest; // 追加：並び替え順を変更
+                _sortFriends(); // 追加：並び替えメソッドを呼び出し
+              });
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: friends.length,
